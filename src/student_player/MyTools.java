23495fe06f6b4182052ae5985c0615a4473932eb
Miya Keilin <miya.keilin@gmail.com>
2020-04-03 11:51:19 -0700

    added movesToGoal
    method to calculate the heuristic for a tile given its position
    takes the tile, its position, and the position of the nugget as input

--- src/student_player/MyTools.java
+++ src/student_player/MyTools.java
@@ -1,7 +1,19 @@
 package student_player;
 
+import Saboteur.cardClasses.SaboteurTile;
+
 public class MyTools {
     public static double getSomething() {
         return Math.random();
+    }
+    // the heuristic for tiles
+    // returns the number of moves to get to the nugget given a tile and its position
+    public int movesToGoal(SaboteurTile tile, int[] pos, int[] nugget) {
+    		int nuggetXPos = nugget[0];
+    		int nuggetYPos = nugget[1];
+    		int tileXPos = pos[0];
+    		int tileYPos = pos[1];
+    		
+    		return 0;
     }
 }
